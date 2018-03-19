dbf10556418b0b4045d069e300a4602d10fc592c
imyzt <imyzt01@email.com>
2018-03-19 16:20:17 +0800

    增加统一异常处理

--- src/main/java/top/imyzt/springboot/advanced/handle/ExceptionHandle.java
+++ src/main/java/top/imyzt/springboot/advanced/handle/ExceptionHandle.java
@@ -17,7 +17,7 @@ public class ExceptionHandle {
      * 统一异常处理方法,执行顺序
      * 抛出异常的方法 -> ExceptionHandle.handle()捕获异常,通过ResultUtil.error()将消息以@ResponseBody注解的JSON数据返回
      * @param e 异常对象
-     * @return
+     * @return 返回错误对象
      */
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
